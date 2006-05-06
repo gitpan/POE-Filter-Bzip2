1548f70f7ef67e1e87e693591e1094223530b72f
Chris Williams <chris@bingosnet.co.uk>
2006-05-06 14:47:29 +0000

    Import of BINGOS/POE-Filter-Bzip2-1.1 from CPAN.
    gitpan-cpan-distribution: POE-Filter-Bzip2
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         BINGOS/POE-Filter-Bzip2-1.1.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Filter/Bzip2.pm
+++ lib/POE/Filter/Bzip2.pm
@@ -3,8 +3,9 @@ package POE::Filter::Bzip2;
 use Carp;
 use Compress::Bzip2 qw(compress decompress);
 use vars qw($VERSION);
+use base qw(POE::Filter);
 
-$VERSION = '1.0';
+$VERSION = '1.1';
 
 sub PUT_LITERAL () { 1 }
 
@@ -21,8 +22,8 @@ sub new {
 }
 
 sub level {
-  my ($self) = shift;
-  my ($level) = shift;
+  my $self = shift;
+  my $level = shift;
 
   if ( defined ( $level ) ) {
 	$self->{level} = $level;
@@ -57,7 +58,7 @@ sub get_one_start {
 }
 
 sub get_one {
-  my ($self) = shift;
+  my $self = shift;
   my $events = [];
 
   if ( my $raw_line = shift ( @{ $self->{BUFFER} } ) ) {
